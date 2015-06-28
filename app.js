0f5166d596ca9ab4d1edf8e94ccb191e455c8a2b
julianpsm <jpalmerosm@gmail.com>
2015-06-28 20:44:58 +0200

    Vistas parciales y marco

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 
@@ -13,6 +14,8 @@ var app = express(); //se crea aplicación
 // view engine setup, instala generador de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 
 //instala los middlewares a utilizar en la aplicación
