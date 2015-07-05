76b8aee6bb971e11e3864ec8cc6efcddd0eed0b4
julianpsm <jpalmerosm@gmail.com>
2015-07-05 11:04:26 +0200

    Rama busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,12 +14,18 @@ exports.load=function(req,res,next,quizId){
 };
 
 
-//GET /quizes
-exports.index=function(req,res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
-	}
-	).catch(function(error) { next(error);})
+//GET /quizes búsqueda de preguntas
+//El patrón buscado es reemplazado por %, siendo insensible a mayúsculas 
+
+exports.index = function( req, res){
+
+	if(req.query.search){
+		var search = '%' + req.query.search.replace(/\s/gi, "%") + '%';
+	} else {var search = '%'};
+	
+	models.Quiz.findAll({where: ["upper(pregunta) like upper(?)", search], order: 'pregunta ASC'}).then(function(quizes) {
+		res.render('quizes/index', {quizes: quizes});
+	}).catch(function(error) {next(error);});
 };
 
 
