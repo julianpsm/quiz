653e6cdc22a7054a5fee24a533fd700913d4cfb3
julianpsm <jpalmerosm@gmail.com>
2015-07-07 23:56:46 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,12 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load); //autoload :quizId
-
+//Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 router.get('/credits/author', authorController.author);
 
